e4cefa1cd82af94afd0b14e1c4b640bf5c8c8b2c
v7-desenvolvedor <antoniolfreitas1965@gmail.com>
2026-05-01 21:24:39 -0300

    Update gui.h

--- app/src/main/cpp/samp/gui/gui.h
+++ app/src/main/cpp/samp/gui/gui.h
@@ -22,7 +22,7 @@
 #include "samp_widgets/chat.h"
 #include "samp_widgets/spawn.h"
 #include "samp_widgets/buttonpanel.h"
-#include "samp_widgets/playerTabList.h"
+#include "samp_widgets/playertablist.h"
 #include "samp_widgets/voicebutton.h"
 
 #include "samp_widgets/dialogs/dialog.h"
